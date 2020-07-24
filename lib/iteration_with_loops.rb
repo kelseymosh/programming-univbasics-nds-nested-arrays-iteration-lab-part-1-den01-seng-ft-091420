fc6525160ffb69dacc1bd96051bb40962a543ef5
Kelsey Oshiro <kelseymosh@gmail.com>
2020-07-24 00:36:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,11 @@
 def find_even_values(src)
-array_of_arrays = [
-   [10, 11],
-   [99, 50, 3, 4],
-   [23, 41]
- ]
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      puts src[row_index][element_index]
+      element_index += 1
+    end
+    row_index += 1
+  end
 end
